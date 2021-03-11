160c44d5f727d800663666685ff31e71b3bc86d3
RockyGracie <igorferreira.bjj@gmail.com>
2021-03-11 07:44:21 -0300

    layout fully responsible

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
+// import { useState } from 'react';
+
 import GlobalStyle from './styles/GlobalStyle';
 import Header from './components/Header';
 import Search from './components/Search';
+import Countries from './components/Countries';
 
 const App = () => {
+
+
+
   return (
     <>
       <GlobalStyle />
       <Header />
       <Search />
+      <Countries />
     </>
   );
 };
---
+++ src/components/Countries/styles.js
@@ -0,0 +1,46 @@
+import styled from 'styled-components';
+
+export const CountriesContainer = styled.div`
+   padding: 0 30px;
+   margin: 0 auto;
+
+   @media screen and (min-width: 55rem) {
+      display: grid;
+      grid-template-columns: repeat(2, 1fr)
+   }
+   
+   @media screen and (min-width: 1125px) {
+      grid-template-columns: repeat(3, 1fr)
+   }
+   
+   @media screen and (min-width: 90rem) {
+      grid-template-columns: repeat(4, 1fr)
+   }
+`
+
+export const Country = styled.div`
+   display: flex;
+   flex-direction: column;
+
+   width: 20.625rem;
+   height: 25rem;
+   margin: 0 auto 3.125rem;
+   box-shadow: 0 .5rem .5rem rgba(0, 0, 0, .1);
+
+   img {
+      flex: 50%;
+      width: 100%;
+      height: 100%;
+   }
+`
+
+export const CountryInfo = styled.div`
+   flex: 50%;
+   padding: 1.875rem;
+
+   h3 { margin-bottom: 0.75rem }
+
+   p span { font-weight: 600 }
+
+   p:not(:last-child) { margin-bottom: 0.3125rem }
+` 
