09df587e9a47610396e05154e78d4fce72bd6289
RockyGracie <igorferreira.bjj@gmail.com>
2021-03-11 15:54:31 -0300

    added dark mode feature

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,12 @@
 import { HeaderStyle } from './styles';
 import { FaRegMoon, FaMoon } from 'react-icons/fa';
 
-const Header = () => {
+const Header = ({ themeToggler }) => {
    return (
       <>
       <HeaderStyle>
          <p>Where in the world?</p>
-         <button>
+         <button onClick={() => themeToggler()}>
             <FaRegMoon style={{marginRight: '0.625rem'}} /> 
             Dark Mode
          </button>
